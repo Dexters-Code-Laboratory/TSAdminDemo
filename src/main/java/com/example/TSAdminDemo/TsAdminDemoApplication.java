9c17e4fc4dc4d3c981eff652f2e7ba06968d9b47
Tushar Saxena <tushar.saxena@outlook.com>
2019-10-02 01:05:40 +0530

    Initial Commit

---
+++ src/main/java/com/example/TSAdminDemo/TsAdminDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TSAdminDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsAdminDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsAdminDemoApplication.class, args);
+	}
+
+}
